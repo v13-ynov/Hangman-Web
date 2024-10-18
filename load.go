08268b6fc6f7e4409c6f9fb873ba7c9398e362af
v13-ynov <gabriellaugier04@gmail.com>
2024-10-18 09:55:30 +0200

    handlers

--- load.go
+++ load.go
@@ -8,27 +8,11 @@ import (
 
 const port = ":8080"
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "Home Page")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "About Page")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	t, err := template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	t.Execute(w, nil)
-}
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
-	fmt.Println(" (http://localhost:8080) - Server Start on port : ", port)
+	fmt.Println(" (http://localhost:8080) - Server Start on port :", port)
 	http.ListenAndServe(port, nil)
 }
